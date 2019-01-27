04fcf03945353277acbfa628c6afb623ca348b0b
Pahul Panesar <pahulpn@gmail.com>
2019-01-26 23:38:05 -0800

    got shared list

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,22 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const token = 'BQCim05-9AH1BryxdJbUtRdn7hNFDaF2sAdq7NhPH2X5BUuPqndhqNVK_IAdmy3tVhnPCbqODd-cQ-TFblU';
-
+const token1 = 'BQBfpL-Fm6Z5n8N3pdKsVR8OGt6-Hwk-QAPHGdHxajj0DCitmw1FlPUgu0QoK-a_h6AYUJMO8hEc3H2wUxzYuKIVSM2maUL9MvgoMo8F2ey6cLOCBtVNkaDqhF5kL2oiLG1KRWUnqsLUKizyJzFT1ziZLRqf0Jj4QZvspnzWus3zU8gYTrS_n7TFi6T8u28jn1dGfaphUek1DqLGIXLwmSRBYH2CTxeqfL_Ggf7PkJc_ebHJCNyb6CPNem1xSlbGaml8tTxxUOlnNwQlkxp3';
+const token2 = 'BQCBnvqwIdlByZF9nQY8FA4bYep4pZu95mrz0QDQ9tnYqB5iOMmKxG4TbCZ-h4ztNTrEOpWB-eqvCESOuAjToGybtwqOI7CPZofrhe1vM_Rzi9X36LWPKDsQbgvTdKnRIk0p4ou4lVPvlh8ShZlJXwTh30zbvLseirMfmmaC3w7lTAjtVZxv3OA5MXG_T2eBTDpdNsfYKlbCwmsYhCPIKhdZ71JKmCS8jSFHpXBP'
+const URIArrays = [];
+const tokens = [];
+const finalSongList = [];
+tokens.push(token1);
+tokens.push(token2);
 // API endpoint - playlist generation
 // req: array of usernames
 // res: success/error message
 app.get('/playlist', (req, res) => {
-  const tokens = [];
-  
+ 
   // get list of songs shared
-  const songs = getSharedSongs();
+  for(i = 0; i < tokens.length; i++){
+    getSharedSongs(tokens[i])
+  }
 
   // // create new spotify playlist
   // const username = 'karnrahal';
@@ -42,7 +48,7 @@ app.listen(port, () => console.log(`Shuffle listening on port ${port}!`));
  * params: Array[String] tokens
  * returns: Array[String] songs
  */
-const getSharedSongs = (tokens) => {
+const getSharedSongs = (token) => {
   // options to get users top 50 tracks
   const options = {
     url: 'https://api.spotify.com/v1/me/top/tracks',
@@ -50,7 +56,7 @@ const getSharedSongs = (tokens) => {
       'Authorization': 'Bearer ' + token
     },
     qs: {
-      'limit': 2
+      'limit': 50
     }
   };
   
@@ -64,9 +70,44 @@ const getSharedSongs = (tokens) => {
 
     // get top 50 songs for each user and put into seperate arrays
     songs = JSON.parse(body).items;
+    parsed = parseSongs(songs)
   });
-  
+
   return songs;
+}
+
+const parseSongs = (songsBody) => {
+  count = 0;
+  URIArray = [];
+  for(i = 0; i < songsBody.length; i++){
+    count++;
+    songBody = songsBody[i];
+    uri = songBody.uri;
+    URIArray.push(uri.substring(uri.indexOf('track:')+6))
+  }
+  if(count == songsBody.length){
+    URIArrays.push(URIArray);
+  }
+  if(URIArrays.length == tokens.length){
+    uarr = URIArrays[0];
+    for(i = 0; i < uarr.length; i++){
+      song = uarr[i];
+      for(j = 1; j < URIArrays.length; j++){
+        found = [];
+        for(k = 0; k < URIArrays[j].length; k++){
+          searchSong = URIArrays[j][k];
+          if(searchSong === song){
+            found.push(1)
+          }
+        }
+        if(found.length == URIArrays.length-1){
+          finalSongList.push(song);
+        }
+        found = [];
+      }
+    }
+  }
+  console.log("lol");
 }
 
 /*
